ead0a08be5aa72d16915c8e204c61edb325084d7
Suraj Kumar <sk5195609@gmail.com>
2024-10-09 15:12:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://guftgu-chatapp-frontend.onrender.com",
     credentials: true,
   },
 });
