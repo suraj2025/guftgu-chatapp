4dc1f1a9693356e3a7b947c411be47415d1cbb47
Suraj Kumar <sk5195609@gmail.com>
2025-04-22 10:42:21 +0530

    Update SetAvatar.jsx

--- client/src/components/SetAvatar.jsx
+++ client/src/components/SetAvatar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import multiavatar from "@multiavatar/multiavatar/esm";
 import loader from "../assets/loader.gif";
